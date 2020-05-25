1475cf40c6f42096418601eca6acb5ab29be2f00
sanzhixiong19860117 <770958429@qq.com>
2020-05-25 21:29:15 +0800

    对选择排序进行的注视的增加

--- dya01/code/src/com/joy/sort/SelectDemo.java
+++ dya01/code/src/com/joy/sort/SelectDemo.java
@@ -1,5 +1,7 @@
 package com.joy.sort;
 
+import java.util.Arrays;
+
 /**
  * @author joy
  * @date 2020/5/25
@@ -9,9 +11,7 @@ public class SelectDemo {
         //选择排序
         int[] arr = {1,3,55,2,99,33,22};
         selectSort(arr);
-        for (int i = 0; i <arr.length ; i++) {
-            System.out.println(arr[i]);
-        }
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void selectSort(int[] a){
@@ -20,15 +20,20 @@ public class SelectDemo {
             return;
         }
 
-        for (int i = 0; i <a.length-1; i++) {
-            int min = i;
+        //选择排序
+        //0-n-1
+        for (int i = 0; i < a.length -1 ; i++) {
+            int min = i;//这里是最小的数为当前0
             for (int j = i+1; j <a.length ; j++) {
+                //这个循环就是最小的i+1 到n这里的数据进行循环
                 if(a[j]<a[min]){
+                  //与最小数据进行对比,如果满足条件就把下表进行给予
                     min = j;
                 }
             }
-
+            //判断min和i是否相当
             if(min != i){
+                //进行数据的替换
                 int tmp = a[i];
                 a[i] = a[min];
                 a[min] = tmp;
